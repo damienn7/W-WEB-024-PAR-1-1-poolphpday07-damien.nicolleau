914fd7a411ebbefa043f4d2231c71e7343c2c916
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-30 10:08:35 +0100

    Calcul de la factorielle des nombres entiers testée, fonctionne

---
+++ ex_01.php
@@ -0,0 +1,24 @@
+<?php
+
+
+function  my_facto_rec($nbr)
+{
+    if($nbr>0 && is_int($nbr))
+    {
+        $return = 1;
+        for ($i=2; $i <= $nbr; $i++) {
+            $return *= $i;
+        }
+        return $return;
+    }
+    elseif($nbr===0)
+    {
+        return 1;
+    }
+    else
+    {
+        return NUll;
+    }
+}
+
+//var_dump(my_facto_rec(7));
