b539f634638f5a81ed8eae491f6c9b86603dc0ef
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-30 21:57:16 +0100

    renvoie true si le nombre passé en paramètre est un nombre premier

--- ex_09.php
+++ ex_09.php
@@ -1,2 +1,28 @@
 <?php
 
+function is_prime($nbr)
+{
+    if(!isset($nbr) || !is_int($nbr) || $nbr<0)
+    {
+        return NULL;
+    }
+
+    if($nbr==2)
+    {
+        return true;
+    }
+    
+    for($i=2;$i<$nbr;$i++)
+    {
+        if($nbr%$i==0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+
+//var_dump(is_prime(2));
