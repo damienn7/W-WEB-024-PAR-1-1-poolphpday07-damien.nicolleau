fe8a51ac1810449c78162bdeb35a83d344b84da7
damienn7 <118727505+damienn7@users.noreply.github.com>
2022-11-30 17:44:28 +0100

    push de l'exercice avec l'explode de chaines de caractères

---
+++ ex_07.php
@@ -0,0 +1,19 @@
+<?php
+
+function str_to_wordtab($str, $delim)
+{
+    return explode($delim,$str);
+}
+
+//var_dump(str_to_wordtab("dedhji dezduhd azdbhjzd dzdhkjz"," "));
+
+/*
+Écrire une fonction qui prend en paramètres une chaîne de caractères et un délimiteur.
+Elle doit retourner un tableau contenant les morceaux de la chaîne découpée grâce au délimiteur.
+En cas d’erreur, si les arguments ne sont pas du bon type ou si la fonction est appelée sans argument, la
+fonction devra retourner NULL.
+
+str_to_wordtab(“Mais tu es tout la”, ’ ’ );
+// retourne array(“Mais”, “tu”, “es”, “tout”, “la”)
+
+*/
